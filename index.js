03cb3829eb243ac8eeff25aab0bd7e276f3f2d6e
chris <chrischau94@gmail.com>
2020-03-02 02:56:27 -0700

    some commands

--- index.js
+++ index.js
@@ -27,15 +27,33 @@ io.on('connection', function(socket){
     var user = {
         socket: socket.id,
         name: assignName(io.engine.clientsCount),
+        color: "green",
     };
     socket.on('chat message', function(msg) {
         var today = new Date();
         var hours = today.getHours() % 12 || 12
         var minutes = (today.getMinutes() < 10) ? "0"+today.getMinutes() : today.getMinutes();
     
-        io.emit('chat message', user.name + " " + hours  + ":" + minutes + " - " + msg);
 
-        /* Check message for chat commands
+        // Should be the command 
+        var chat_commands = msg.split(" ")[0];
+        // The value for the command
+        var command_value = msg.split(" ")[1]
+
+        /* Chat commands */
+        if (chat_commands === "/nick") {
+            user.name = command_value;
+            console.log("changed nickname from " + user.name + "to " + command_value);
+        } else if (chat_commands === "/nickcolor") {
+            user.color = command_value;
+        } else if (chat_commands.charAt(0) === "/") {
+            console.log("invalid output")
+            socket.emit('chat message', 'Invalid command, please try again');
+        } else {
+            io.emit('chat message', user.name + " " + hours  + ":" + minutes + " - " + msg);
+        }
+
+
     });
 });
 
