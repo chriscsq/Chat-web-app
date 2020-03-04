eecb381ce1fbed1e1e4aa782d73e9d2cf471d298
chrischau94@gmail.com <chrischau94@gmail.com>
2020-03-03 23:26:25 -0700

    no progress made really

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ var http = require('http').createServer(app);
 var io = require('socket.io')(http);
 
 
+
 //app.use(cookieParser());
 
 app.use(express.static(__dirname + '/public'));
@@ -12,23 +13,23 @@ app.get('/', function(req, res) {
     res.sendFile(__dirname + '/index.html');
 });
 
-app.get('/', function(req, res) {
-    res.send('<h1>Hello world </h1>');
-});
 
 http.listen(3000, function() {
     console.log('listening on *:3000');
 });
 
-
+var users = new Array()
 io.on('connection', function(socket){
 
     console.log(socket.id + " connected");
     var user = {
         socket: socket.id,
         name: assignName(io.engine.clientsCount),
-        color: "green",
-    };
+        color: "black",
+    }
+
+    users.push(user);
+
     socket.on('chat message', function(msg) {
         var today = new Date();
         var hours = today.getHours() % 12 || 12
@@ -36,31 +37,62 @@ io.on('connection', function(socket){
     
 
         // Should be the command 
-        var chat_commands = msg.split(" ")[0];
+        var chat_commands = msg.split(" ")[0]
         // The value for the command
         var command_value = msg.split(" ")[1]
+        var invalid_input = msg.split(" ")[2]
 
+        console.log('invalid input: ' + invalid_input)
         /* Chat commands */
         if (chat_commands === "/nick") {
-            user.name = command_value;
-            console.log("changed nickname from " + user.name + "to " + command_value);
+            if (isInvalidInput(invalid_input)){
+                socket.emit('chat message', "Invalid input, please check your command")
+            } else if (isNameAvailable(command_value) === true) {
+                user.name = command_value
+            } else {
+                socket.emit('chat message', "Name already taken")
+            }
         } else if (chat_commands === "/nickcolor") {
-            user.color = command_value;
+            if (isInvalidInput(invalid_input)) {
+                socket.emit('chat message', "Invalid input, please check your command")
+            } else {
+                user.color = command_value;
+            }
         } else if (chat_commands.charAt(0) === "/") {
-            console.log("invalid output")
             socket.emit('chat message', 'Invalid command, please try again');
         } else {
             io.emit('chat message', user.name + " " + hours  + ":" + minutes + " - " + msg);
         }
 
 
+        console.log(users);
     });
 });
 
-function changeName(user, name) {
-    user.name = name
+function assignName(id) {
+    var username = "User"+id
+    while (!isNameAvailable(username)) {
+        username = "User"+(id++)
+        console.log("incrementing user id")
+        console.log("Name will be: User"+(id++))
+    }
+    return username
 }
 
-function assignName(id) {
-    return "User " + id;
+function isInvalidInput(invalid_input) {
+    if (typeof invalid_input === 'undefined') {
+        return false
+    }
+    return true
 }
+function isNameAvailable(name_to_check) {
+    for (var i = 0; i < users.length; i++) {
+        if (users[i].name === name_to_check) {
+            console.log("check name availability failed")
+            return false;
+        }
+    }
+    return true;
+}
+
+
--- public/js/functions.js
+++ public/js/functions.js
@@ -2,15 +2,16 @@ $(document).ready(function() {
     //var socket = io();
     var socket = io.connect('http://localhost:3000');
     $('form').submit(function(e) {
-        var today = new Date();
         e.preventDefault();
-        socket.emit('chat message', $('#m').val());
+        var message = $('#m').val()
+        socket.emit('chat message', message );
         $('#m').val('');
         return false;
+
     });
 
     socket.on('chat message', function(msg) {
         
-        $('#messages').append($('<li>').text(msg));
+        $('#messages').append($('<li>').text(msg).css("color","green"));
     });
 });
