345930128ec003276c972e86fb48a2714d3c85bd
chriscsq <chrischau94@gmail.com>
2020-03-06 13:30:58 -0700

    prettier formatting

--- public/js/client.js
+++ public/js/client.js
@@ -1,102 +1,114 @@
-let name
-let color
-let userlist
+let name;
+let color;
+let userlist;
 
 $(document).ready(function() {
-    //let socket = io();
+  let socket = io.connect("http://localhost:3000");
+  $("form").submit(function(e) {
+    e.preventDefault();
+    let msg = $("#m").val();
+    socket.emit("chat message", msg);
+    $("#m").val("");
 
-    let socket = io.connect('http://localhost:3000');
-    $('form').submit(function(e) {
-        e.preventDefault();
-        let msg = $('#m').val()
-        socket.emit('chat message', msg);
-        $('#m').val('');
+    return false;
+  });
 
-        return false;
-    });
+  if (name != "undefined") {
+    name = Cookies.get("user");
+  }
 
-    if (name != "undefined") {
-        name = Cookies.get('user')
+  socket.on("connect", function() {
+    let cookie = Cookies.get("user");
+    socket.emit("cookiehandler", cookie);
+  });
+
+  socket.on("populate_history", function(chathistory) {
+    $("#messages").empty();
+    for (let i = 0; i < chathistory.length; i++) {
+      $("#messages").append($("<li>").text(chathistory[i]));
+    }
+    $("#messages").scrollTop($("#messages")[0].scrollHeight);
+  });
+
+  socket.on("disconnected", function(data) {
+    console.log("disconnect event");
+    userlist = data;
+
+    $("#users").empty();
+    for (let i = 0; i < userlist.length; i++) {
+      $("#users").append($("<li>").text(userlist[i]));
+    }
+  });
+
+  socket.on("send_user_list", function(data) {
+    userlist = data;
+    $("#users").empty();
+    for (let i = 0; i < userlist.length; i++) {
+      $("#users").append($("<li>").text(userlist[i]));
+    }
+  });
+
+  socket.on("connected", function(data) {
+    userlist = data.userlist;
+    $("#chat-header").text("Your name is: " + data.name);
+    Cookies.set("user", data.name, { expires: 365 });
+    $("#users").empty();
+    for (let i = 0; i < userlist.length; i++) {
+      $("#users").append($("<li>").text(userlist[i]));
+    }
+  });
+
+  socket.on("chat message", function(msg) {
+    let hex = "#" + msg.color;
+    if (socket.id === msg.socketid) {
+      $("#messages").append(
+        '<li><span class="msgfrom" style="color:' +
+          hex +
+          ';font-weight:bold" id="' +
+          msg.socketid +
+          '">' +
+          msg.name +
+          "</span>" +
+          msg.messagecontent +
+          "</li>"
+      );
+    } else {
+      $("#messages").append(
+        '<li><span class="msgfrom" style="color:' +
+          hex +
+          ';" id="' +
+          msg.socketid +
+          '">' +
+          msg.name +
+          "</span>" +
+          msg.messagecontent +
+          "</li>"
+      );
     }
 
-    socket.on('connect', function() {
-        let cookie = Cookies.get('user')
-        socket.emit('cookiehandler', cookie)
+    $("#messages").scrollTop($("#messages")[0].scrollHeight);
+  });
 
-    })
+  socket.on("error message", function(error) {
+    $("#messages").append(
+      $("<li>")
+        .text(error)
+        .css("color", "red")
+    );
+    $("#messages").scrollTop($("#messages")[0].scrollHeight);
+  });
 
+  socket.on("serverToClient", function(data) {
+    name = data.name;
+    color = data.color;
+    userlist = data.userlist;
+    $("#users").empty();
+    console.log("dataname in servertoclient " + data.name);
+    Cookies.set("user", data.name, { expires: 365 });
 
-
-    socket.on('populate_history', function(chathistory) {
-        $('#messages').empty()
-        for (let i = 0; i < chathistory.length; i++) {
-            $('#messages').append($('<li>').text(chathistory[i]))
-            
-        }
-        $("#messages").scrollTop($("#messages")[0].scrollHeight);
-
-    });
-
-    socket.on('disconnected', function(data) {
-        console.log('disconnect event')
-        userlist = data
-
-        $('#users').empty()
-        for (let i = 0; i < userlist.length; i++) {
-            $('#users').append($('<li>').text(userlist[i]))
-        }
-    });
-
-    socket.on('send_user_list', function(data) {
-        userlist = data
-        $('#users').empty()
-        for (let i = 0; i < userlist.length; i++) {
-            $('#users').append($('<li>').text(userlist[i]))
-        }
-    });
-
-    socket.on('connected', function(data) {
-        userlist = data.userlist
-        $('#chat-header').text("Your name is: " + data.name)
-        Cookies.set('user', data.name, {expires: 365})
-        $('#users').empty()
-        for (let i = 0; i < userlist.length; i++) {
-            $('#users').append($('<li>').text(userlist[i]))
-        }
-    });
-
-    socket.on('chat message', function(msg) {
-        let hex = "#"+msg.color
-        if (socket.id === msg.socketid) {
-            $('#messages').append('<li><span class="msgfrom" style="color:'+hex+';font-weight:bold" id="'+msg.socketid+'">' + msg.name + '</span>' + msg.messagecontent + '</li>')
-
-        } else {
-            $('#messages').append('<li><span class="msgfrom" style="color:'+hex+';" id="'+msg.socketid+'">' + msg.name + '</span>' + msg.messagecontent + '</li>')
-
-        }
-        
-        $("#messages").scrollTop($("#messages")[0].scrollHeight);
-    });
-
-    socket.on('error message', function(error) {
-        $('#messages').append($('<li>').text(error).css('color','red'))
-        $("#messages").scrollTop($("#messages")[0].scrollHeight);
-
-    })
-
-    socket.on('serverToClient', function(data) {
-        name = data.name
-        color = data.color
-        userlist = data.userlist
-        $('#users').empty()
-        console.log('dataname in servertoclient ' + data.name)
-        Cookies.set('user', data.name, {expires: 365})
-    
-        $('#chat-header').text("Your name is: " + name)
-        for (let i = 0; i < userlist.length; i++) {
-            $('#users').append($('<li>').text(userlist[i]))
-        }
-    });
-
+    $("#chat-header").text("Your name is: " + name);
+    for (let i = 0; i < userlist.length; i++) {
+      $("#users").append($("<li>").text(userlist[i]));
+    }
+  });
 });
-
--- server.js
+++ server.js
@@ -1,227 +1,217 @@
-let express = require('express');
+let express = require("express");
 let app = express();
-let http = require('http').createServer(app);
-let io = require('socket.io')(http);
+let http = require("http").createServer(app);
+let io = require("socket.io")(http);
 
 //app.use(cookieParser());
-app.use(express.static(__dirname + '/public'));
+app.use(express.static(__dirname + "/public"));
 
-app.get('/', function(req, res) {
-    res.sendFile(__dirname + '/index.html');
+app.get("/", function(req, res) {
+  res.sendFile(__dirname + "/index.html");
 });
-
 
 http.listen(3000, function() {
-    console.log('listening on *:3000');
+  console.log("listening on *:3000");
 });
 
-let users = new Array()
-let history = new Array()
+let users = new Array();
+let history = new Array();
 
+io.on("connection", function(socket) {
+  let user = {
+    socket: socket.id,
+    name: assignName(1),
+    color: "black"
+  };
 
-io.on('connection', function(socket){
+  users.push(user);
+  let userlist = refreshUserList(socket.id, "connect", "");
+  io.to(`${socket.id}`).emit("connected", {
+    userlist: userlist,
+    name: user.name
+  });
 
-    let user = {
-        socket: socket.id,
-        name: assignName(1),
-        color: "black",
-    }
+  io.to(`${socket.id}`).emit("populate_history", history);
+  io.emit("send_user_list", userlist);
 
-    users.push(user)
-    let userlist = refreshUserList(socket.id, "connect", "")
-    //io.emit('connected', userlist)
-    io.to(`${socket.id}`).emit('connected', {
-        userlist: userlist,
+  let username = user.name;
+  socket.on("cookiehandler", function(data) {
+    if (data == null) {
+      //user.name = assignName(1)
+      io.to(`${socket.id}`).emit("serverToClient", {
         name: user.name,
-    });
+        color: user.color,
+        userlist: refreshUserList(socket.id, "changenick", user.name)
+      });
+    } else if (data != user.name) {
+      /* See if name is taken during disconnect */
+      if (isNameAvailable(data)) {
+        user.name = data;
+      } else {
+        user.name = assignName(1);
+      }
 
-    io.to(`${socket.id}`).emit('populate_history', history)
-    io.emit('send_user_list', userlist)
+      io.to(`${socket.id}`).emit("serverToClient", {
+        name: user.name,
+        color: user.color,
+        userlist: refreshUserList(socket.id, "changenick", user.name)
+      });
+      io.emit(
+        "send_user_list",
+        refreshUserList(socket.id, "changenick", user.name)
+      );
+    }
+  });
 
-    let username = user.name
+  socket.on("disconnect", function() {
+    io.emit("disconnected", refreshUserList(socket.id, "disconnect", ""));
+    for (let i = 0; i < users.length; i++) {
+      if (users[i].socket == socket.id) {
+        users.splice(i, 1);
+      }
+    }
+    //userlist = refreshUserList(socket.id, "disconnect");
+  });
+  socket.on("chat message", function(msg) {
+    let today = new Date();
+    let hours = today.getHours() % 12 || 12;
+    let minutes =
+      today.getMinutes() < 10 ? "0" + today.getMinutes() : today.getMinutes();
+
     /*
-    socket.on('connected', function() {
-        console.log("this should work")
-        console.log("username: " + username)
-        socket.emit('connected', username)
-    });
-    */
+     * User input
+     */
+    let chat_commands = msg.split(" ")[0];
+    let command_value = msg.split(" ")[1];
+    let invalid_input = msg.split(" ")[2];
 
-    socket.on('cookiehandler', function(data) {
+    /*
+     * Command to change nickanme
+     */
+    if (chat_commands === "/nick") {
+      if (isInvalidInput(invalid_input)) {
+        socket.emit(
+          "error message",
+          "Invalid input, please check your command"
+        );
+      } else if (isNameAvailable(command_value) === true) {
+        user.name = command_value;
 
-        if (data == null) {
-            //user.name = assignName(1)
-            io.to(`${socket.id}`).emit('serverToClient', {
-                name: user.name,
-                color: user.color,
-                userlist: refreshUserList(socket.id, "changenick", user.name),
-            });
+        io.to(`${socket.id}`).emit("serverToClient", {
+          name: user.name,
+          color: user.color,
+          userlist: refreshUserList(socket.id, "changenick", user.name)
+        });
 
-        } else if (data != user.name) {
-            /* See if name is taken during disconnect */
-            if (isNameAvailable(data)) {
-                user.name = data
-            } else {
-                user.name = assignName(1)
-            }
+        io.emit(
+          "send_user_list",
+          refreshUserList(socket.id, "changenick", user.name)
+        );
+      } else {
+        socket.emit("error message", "Name already taken");
+      }
 
-            io.to(`${socket.id}`).emit('serverToClient', {
-    
-                name: user.name,
-                color: user.color,
-                userlist: refreshUserList(socket.id, "changenick", user.name),
-                
-            });
-            io.emit('send_user_list', refreshUserList(socket.id, "changenick", user.name))
+      /*
+       * Nickcolor command, checks for valid hexcolor
+       */
+    } else if (chat_commands === "/nickcolor") {
+      if (isInvalidInput(invalid_input) || !isHexColor(command_value)) {
+        socket.emit(
+          "error message",
+          "Invalid input, please check your command"
+        );
+      } else {
+        user.color = command_value;
+        io.to(`${socket.id}`).emit("serverToClient", {
+          name: user.name,
+          color: user.color,
+          userlist: refreshUserList(socket.id, "changecolor", user.name)
+        });
+      }
+    } else if (chat_commands.charAt(0) === "/") {
+      socket.emit("error message", "Invalid command, please try again");
+    } else {
+      /* Does not send empty messages */
+      if (msg != "") {
+        color = user.color;
+        socketid = user.socket;
+        name = user.name;
+        let savedmessage = name + " " + hours + ":" + minutes + " - " + msg;
 
-        }
-    });
-
-    socket.on('disconnect', function() {
-        io.emit('disconnected', refreshUserList(socket.id, "disconnect", ""))
-        for (let i = 0; i < users.length; i++) {
-            if (users[i].socket == socket.id) {
-                users.splice(i, 1)
-            }
-        }
-        //userlist = refreshUserList(socket.id, "disconnect");
-    });
-    socket.on('chat message', function(msg) {
-        let today = new Date();
-        let hours = today.getHours() % 12 || 12
-        let minutes = (today.getMinutes() < 10) ? "0"+today.getMinutes() : today.getMinutes();
-    
-        /* 
-         * User input
-         */
-        let chat_commands = msg.split(" ")[0]
-        let command_value = msg.split(" ")[1]
-        let invalid_input = msg.split(" ")[2]
-
-        /* 
-         * Command to change nickanme
-         */
-        if (chat_commands === "/nick") {
-            if (isInvalidInput(invalid_input)){
-                socket.emit('error message', "Invalid input, please check your command")
-            } else if (isNameAvailable(command_value) === true) {
-                user.name = command_value
-
-                io.to(`${socket.id}`).emit('serverToClient', {
-                    name: user.name,
-                    color: user.color,
-                    userlist: refreshUserList(socket.id, "changenick", user.name),
-                });
-
-                io.emit('send_user_list', refreshUserList(socket.id, "changenick", user.name))
-                
-            } else {
-                socket.emit('error message', "Name already taken")
-            }
-
-        /* 
-         * Nickcolor command, checks for valid hexcolor 
-         */
-
-        } else if (chat_commands === "/nickcolor") {
-            if (isInvalidInput(invalid_input) || !isHexColor(command_value)) {
-                socket.emit('error message', "Invalid input, please check your command")
-            } else {
-                user.color = command_value;
-                io.to(`${socket.id}`).emit('serverToClient', {
-                    name: user.name,
-                    color: user.color,
-                    userlist: refreshUserList(socket.id, "changecolor", user.name),
-
-                });
-            }
-        } else if (chat_commands.charAt(0) === "/") {
-            socket.emit('error message', 'Invalid command, please try again');
-        } else {
-            /* Does not send empty messages */
-            if (msg != "") {
-                color = user.color;
-                socketid = user.socket;
-                name = user.name;
-                let savedmessage = name +  " " + hours  + ":" + minutes + " - " + msg
-                
-                io.emit('chat message',  {
-                    color: user.color,
-                    socketid: user.socket,
-                    name: user.name,
-                    messagecontent: " " + hours  + ":" + minutes + " - " + msg,
-                });
-                history.push(savedmessage)
-
-            }
-        }
-        refreshUserList();
-    });
+        io.emit("chat message", {
+          color: user.color,
+          socketid: user.socket,
+          name: user.name,
+          messagecontent: " " + hours + ":" + minutes + " - " + msg
+        });
+        history.push(savedmessage);
+      }
+    }
+    refreshUserList();
+  });
 });
 
 function assignName(id) {
-    let username = "User"+id
-    while (!isNameAvailable(username)) {
-        username = "User"+(id++)
-    }
-    return username
+  let username = "User" + id;
+  while (!isNameAvailable(username)) {
+    username = "User" + id++;
+  }
+  return username;
 }
 
 function isInvalidInput(invalid_input) {
-    if (typeof invalid_input === 'undefined') {
-        return false
-    }
-    return true
+  if (typeof invalid_input === "undefined") {
+    return false;
+  }
+  return true;
 }
 
 function isNameAvailable(name_to_check) {
-    for (let i = 0; i < users.length; i++) {
-        if (users[i].name === name_to_check) {
-            return false;
-        }
+  for (let i = 0; i < users.length; i++) {
+    if (users[i].name === name_to_check) {
+      return false;
     }
-    return true;
+  }
+  return true;
 }
-
 
 // Stole function to validate hex - https://stackoverflow.com/questions/8027423/how-to-check-if-a-string-is-a-valid-hex-color-representation/8027444
-function isHexColor (hex) {
-    return typeof hex === 'string'
-        && hex.length === 6
-        && !isNaN(Number('0x' + hex))
+function isHexColor(hex) {
+  return (
+    typeof hex === "string" && hex.length === 6 && !isNaN(Number("0x" + hex))
+  );
 }
 
-
 function refreshUserList(socketid, action, newnick) {
-    let usernames = []
-    switch (action) {
-        case "connect":
-            for (let i = 0; i < users.length; i++) {
-                usernames.push(users[i].name)
-            }
-            break;
-        case "disconnect":
-            for (let i = 0; i < users.length; i++) {
-                if (users[i].socket != socketid) {
-                    usernames.push(users[i].name)
-                }
-            }
-            break;
-        case "changenick":
-            for (let i = 0; i < users.length; i++) {
-                if (socketid === users[i].socket) {
-                    usernames.push(newnick)
-                } else {
-                    usernames.push(users[i].name)
-                }
-            }
-            break;
-        default:
-            for (let i = 0; i < users.length; i++) {
-                usernames.push(users[i].name)
-            }
-            break;
-    }
+  let usernames = [];
+  switch (action) {
+    case "connect":
+      for (let i = 0; i < users.length; i++) {
+        usernames.push(users[i].name);
+      }
+      break;
+    case "disconnect":
+      for (let i = 0; i < users.length; i++) {
+        if (users[i].socket != socketid) {
+          usernames.push(users[i].name);
+        }
+      }
+      break;
+    case "changenick":
+      for (let i = 0; i < users.length; i++) {
+        if (socketid === users[i].socket) {
+          usernames.push(newnick);
+        } else {
+          usernames.push(users[i].name);
+        }
+      }
+      break;
+    default:
+      for (let i = 0; i < users.length; i++) {
+        usernames.push(users[i].name);
+      }
+      break;
+  }
 
-    return usernames;
+  return usernames;
 }
